fce655ac236837c3e19491201bb80320e729c1e0
Amritendu De <amritend@in.ibm.com>
2020-05-19 18:58:36 +0530

    Updates

---
+++ src/test/java/com/baeldung/spring/redis/configuration/SpringRedisConfigurationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.baeldung.spring.redis.configuration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRedisConfigurationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
